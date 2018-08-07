8854ea9d6b68ce66b63ee8a1a773e14042e7e603
zoupenghui <zou.penghui@aliyun.com>
2018-08-08 00:06:47 +0800

    api

---
+++ Superhui_API/Controllers/ValuesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Superhui_API.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        ILogger<ValuesController> logger;
+        public ValuesController(ILogger<ValuesController> log)
+        {
+            logger = log;
+        }
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            logger.LogDebug("log msg!");
+            logger.LogInformation("log infomation!");
+            return new string[] { "value", "value2", };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
